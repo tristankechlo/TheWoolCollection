eda7f74eae7255a67d410075a3acd218953fb806
Tristan Kechlo <66692834+tristankechlo@users.noreply.github.com>
2023-02-02 18:29:11 +0100

    make blocks flammable

--- Fabric/src/main/java/com/tristankechlo/more_wool_blocks/FabricMoreWoolBlocks.java
+++ Fabric/src/main/java/com/tristankechlo/more_wool_blocks/FabricMoreWoolBlocks.java
@@ -3,6 +3,7 @@ package com.tristankechlo.more_wool_blocks;
 import com.tristankechlo.more_wool_blocks.init.ModBlocks;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.fabricmc.fabric.api.registry.FlammableBlockRegistry;
 import net.minecraft.world.item.CreativeModeTabs;
 
 public class FabricMoreWoolBlocks implements ModInitializer {
@@ -11,8 +12,14 @@ public class FabricMoreWoolBlocks implements ModInitializer {
     public void onInitialize() {
         MoreWoolBlocks.init();
 
+        // add all blocks to creative tab
         ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.COLORED_BLOCKS).register((content) -> {
             ModBlocks.ALL_ITEMS.forEach(item -> content.accept(item.get()));
+        });
+
+        // make all blocks flammable
+        ModBlocks.ALL_ITEMS.forEach(item -> {
+            FlammableBlockRegistry.getDefaultInstance().add(item.get().getBlock(), 30, 60);
         });
     }
 
--- Forge/src/main/java/com/tristankechlo/more_wool_blocks/ForgeMoreWoolBlocks.java
+++ Forge/src/main/java/com/tristankechlo/more_wool_blocks/ForgeMoreWoolBlocks.java
@@ -2,9 +2,12 @@ package com.tristankechlo.more_wool_blocks;
 
 import com.tristankechlo.more_wool_blocks.init.ModBlocks;
 import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.FireBlock;
 import net.minecraftforge.event.CreativeModeTabEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(MoreWoolBlocks.MOD_ID)
@@ -15,12 +18,20 @@ public class ForgeMoreWoolBlocks {
 
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
         modEventBus.addListener(this::onCreativeModeTabRegister);
+        modEventBus.addListener(this::onCommonSetup);
     }
 
     private void onCreativeModeTabRegister(CreativeModeTabEvent.BuildContents event) {
         if (event.getTab() == CreativeModeTabs.COLORED_BLOCKS) {
             ModBlocks.ALL_ITEMS.forEach(item -> event.accept(item.get()));
         }
+    }
+
+    private void onCommonSetup(FMLCommonSetupEvent event) {
+        ModBlocks.ALL_ITEMS.forEach(item -> {
+            FireBlock fireBlock = (FireBlock) Blocks.FIRE;
+            fireBlock.setFlammable(item.get().getBlock(), 30, 60);
+        });
     }
 
 }
